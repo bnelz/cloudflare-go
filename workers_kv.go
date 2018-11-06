37340e2f7c349e6e261a4551ea94d1984c5c5203
Brandon Nelson <bnelson@cloudflare.com>
2018-11-06 15:10:10 -0800

    Adds create method for worker kv namespace

--- workers_kv.go
+++ workers_kv.go
@@ -1,14 +1,37 @@
 package cloudflare
 
+import (
+	"github.com/pkg/errors"
+)
+
 type WorkerKVNamespace struct {
 	ID    string `json:"id"`
 	Title string `json:"title"`
 }
 
+type workersKVNamespaceResponse struct {
+	Response
+	Result WorkerKVNamespace `json:"result"`
+}
+
 const baseWorkersKVURI = `/storage/kv/namespaces`
 
+// CreateWorkersKVNamespace creates a new worker kv namespace
+// note: titles have a unique constraint within an account
 func (api *API) CreateWorkersKVNamespace(title string) (WorkerKVNamespace, error) {
 	uri := api.userBaseURL("/accounts") + baseWorkersKVURI
+	ns := WorkerKVNamespace{
+		Title: title,
+	}
+	res, err := api.makeRequest("POST", uri, ns)
+	if err != nil {
+		return WorkerKVNamespace{}, errors.Wrap(err, errMakeRequestError)
+	}
+	var r workersKVNamespaceResponse
+	if err := json.Unmarshal(res, &r); err != nil {
+		return WorkerKVNamespace{}, errors.Wrap(err, errUnmarshalError)
+	}
+	return r.Result, nil
 }
 
 func (api *API) ListWorkersKVNamespaces() ([]WorkerKVNamespace, error) {
