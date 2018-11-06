00c143fbf31f5521ccb6ea2cea4eff00024b3eab
Jake Riesterer <jriesterer@cloudflare.com>
2018-11-06 15:15:18 -0800

    add workers kv test stub

---
+++ workers_kv_test.go
@@ -0,0 +1,28 @@
+package cloudflare
+
+import (
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	createWorkerKVNamespaceData = `{}`
+	listWorkerKVNamespaceData   = `{}`
+	renameWorkerKVNamespaceData = `{}`
+	deleteWorkerKVResponseData  = `{}`
+)
+
+func TestCreateWorkersKvNamespace(t *testing.T) {
+	panic("method not implemented")
+}
+
+func TestListWorkersKvNamespaces(t *testing.T) {
+	panic("method not implemented")
+}
+
+func TestRenameWorkersKvNamespace(t *testing.T) {
+	panic("method not implemented")
+}
+
+func TestDeleteWorkersKvNamespace(t *testing.T) {
+	panic("method not implemented")
+}
